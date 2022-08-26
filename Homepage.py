3b7add97c9d16cbcb6e5ce3f67bd0c7984f9bbcf
Krishi Vijayanand <64303145+krishi08@users.noreply.github.com>
2022-08-26 22:30:48 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -5,8 +5,20 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.title("Main Page")
+#st.title("Main Page")
 #st.sidebar.success("Select a page above.")
+a1, a2, a3 = st.columns((3,3,3))
+with a1:
+    # name
+with a2:
+    # age
+with a3:
+    # Type of orthoses
+
+b1, b2 , b3 = st.columns(3)
+b1.metric("Cadence", "73 steps/min", "+8%")
+b2.metric("Stride Length", "1.4 m", "-4%")
+b3.metric("Knee Angle", "8°", "4%")
 
 if "my_input" not in st.session_state:
     st.session_state["my_input"] = ""
